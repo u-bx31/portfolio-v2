3131dd1285bce88b1a7a7ccb9117d9a970bd2fdc
LabchariOussama <oussama.bx31@gmail.com>
2025-07-25 17:29:50 +0100

    edit:nav button varient

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,83 +1,79 @@
 "use client";
 import {
-  Navbar,
-  NavBody,
-  NavItems,
-  MobileNav,
-  NavbarLogo,
-  NavbarButton,
-  MobileNavHeader,
-  MobileNavToggle,
-  MobileNavMenu,
+	Navbar,
+	NavBody,
+	NavItems,
+	MobileNav,
+	NavbarLogo,
+	NavbarButton,
+	MobileNavHeader,
+	MobileNavToggle,
+	MobileNavMenu,
 } from "@/components/ui/resizable-navbar";
+import { Moon } from "lucide-react";
 import { useState } from "react";
 
 export function NavbarDemo() {
-  const navItems = [
-    {
-      name: "Home",
-      link: "#home",
-    },
-    {
-      name: "About",
-      link: "#about",
-    },
-    {
-      name: "Contact",
-      link: "#contact",
-    },
-  ];
+	const navItems = [
+		{
+			name: "Home",
+			link: "#home",
+		},
+		{
+			name: "About",
+			link: "#about",
+		},
+		{
+			name: "Contact",
+			link: "#contact",
+		},
+	];
 
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  return (
-      <Navbar>
-        {/* Desktop Navigation */}
-        <NavBody>
-          <NavbarLogo />
-          <NavItems items={navItems} />
-          <div className="flex items-center gap-4">
-            <NavbarButton variant="secondary">Resume</NavbarButton>
-          </div>
-        </NavBody>
+	return (
+		<Navbar>
+			{/* Desktop Navigation */}
+			<NavBody>
+				<NavbarLogo />
+				<NavItems items={navItems} />
+				<div className="flex items-center  gap-4">
+					<NavbarButton>Resume</NavbarButton>
+					<NavbarButton className="!px-3"><Moon className="w-6 h-6" /></NavbarButton>
+				</div>
+			</NavBody>
 
-        {/* Mobile Navigation */}
-        <MobileNav>
-          <MobileNavHeader>
-            <NavbarLogo />
-            <MobileNavToggle
-              isOpen={isMobileMenuOpen}
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-            />
-          </MobileNavHeader>
+			{/* Mobile Navigation */}
+			<MobileNav>
+				<MobileNavHeader>
+					<NavbarLogo />
+					<MobileNavToggle
+						isOpen={isMobileMenuOpen}
+						onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+					/>
+				</MobileNavHeader>
 
-          <MobileNavMenu
-            isOpen={isMobileMenuOpen}
-            onClose={() => setIsMobileMenuOpen(false)}
-          >
-            {navItems.map((item, idx) => (
-              <a
-                key={`mobile-link-${idx}`}
-                href={item.link}
-                onClick={() => setIsMobileMenuOpen(false)}
-                className="relative text-neutral-600 dark:text-neutral-300"
-              >
-                <span className="block">{item.name}</span>
-              </a>
-            ))}
-            <div className="flex w-full flex-col gap-4">
-              <NavbarButton
-                onClick={() => setIsMobileMenuOpen(false)}
-                variant="primary"
-                className="w-full"
-              >
-                Resume
-              </NavbarButton>
-            </div>
-          </MobileNavMenu>
-        </MobileNav>
-      </Navbar>
-  );
+				<MobileNavMenu
+					isOpen={isMobileMenuOpen}
+					onClose={() => setIsMobileMenuOpen(false)}>
+					{navItems.map((item, idx) => (
+						<a
+							key={`mobile-link-${idx}`}
+							href={item.link}
+							onClick={() => setIsMobileMenuOpen(false)}
+							className="relative text-neutral-600 dark:text-neutral-300">
+							<span className="block">{item.name}</span>
+						</a>
+					))}
+					<div className="flex w-full flex-col gap-4">
+						<NavbarButton
+							onClick={() => setIsMobileMenuOpen(false)}
+							className="w-full">
+							Resume
+						</NavbarButton>
+					</div>
+				</MobileNavMenu>
+			</MobileNav>
+		</Navbar>
+	);
 }
-
-
--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -136,13 +136,13 @@ export const NavItems = ({
 				<a
 					onMouseEnter={() => setHovered(idx)}
 					onClick={onItemClick}
-					className="relative px-4 py-2 text-neutral-600 dark:text-neutral-300"
+					className="relative px-4 py-2 text-text dark:text-neutral-300"
 					key={`link-${idx}`}
 					href={item.link}>
 					{hovered === idx && (
 						<motion.div
 							layoutId="hovered"
-							className="absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-neutral-800"
+							className="absolute inset-0 h-full w-full rounded-full bg-primary/40 dark:bg-neutral-800"
 						/>
 					)}
 					<span className="relative z-20">{item.name}</span>
@@ -277,34 +277,23 @@ export const NavbarButton = ({
 	as: Tag = "a",
 	children,
 	className,
-	variant = "primary",
 	...props
 }: {
 	href?: string;
 	as?: React.ElementType;
 	children: React.ReactNode;
 	className?: string;
-	variant?: "primary" | "secondary" | "dark" | "gradient";
 } & (
 	| React.ComponentPropsWithoutRef<"a">
 	| React.ComponentPropsWithoutRef<"button">
 )) => {
-	const baseStyles =
-		"px-4 py-2 rounded-md bg-white button bg-white text-black text-sm font-bold relative cursor-pointer hover:-translate-y-0.5 transition duration-200 inline-block text-center";
-
-	const variantStyles = {
-		primary:
-			"shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]",
-		secondary: "bg-transparent shadow-none dark:text-white",
-		dark: "bg-black text-white shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]",
-		gradient:
-			"bg-gradient-to-b from-blue-500 to-blue-700 text-white shadow-[0px_2px_0px_0px_rgba(255,255,255,0.3)_inset]",
-	};
-
 	return (
 		<Tag
 			href={href || undefined}
-			className={cn(baseStyles, variantStyles[variant], className)}
+			className={
+				"bg-accent px-5 py-2 rounded-full text-lg font-normal relative cursor-pointer hover:-translate-y-0.5 transition duration-200 inline-block text-center shadow-sm " +
+				className
+			}
 			{...props}>
 			{children}
 		</Tag>
